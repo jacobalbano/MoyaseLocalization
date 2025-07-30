5e4a25b400d3466167287d5293d8a2d3ee11e08d
Jacob Albano <jacob.albano@gmail.com>
2025-07-29 23:04:21 -0400

    Removing index js file; can handle this in the app itself

--- index.js
+++
@@ -1,7 +0,0 @@
-import { map } from 'lodash';
-
-// automatically imports all the modules and exports as a single module object
-const requireModule = require.context('.', false, /\.json$/u);
-export default map(requireModule.keys(), filename =>
-    requireModule(filename).default ?? requireModule(filename)
-);
